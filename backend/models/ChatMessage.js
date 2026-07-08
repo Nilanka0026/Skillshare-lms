60a24fe05255306391914ada2dacd998642b47d2
Sithum Aluvihare <sithumaluvihare@gmail.com>
2026-07-08 19:57:06 +0530

    Chatbot Added

---
+++ backend/models/ChatMessage.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const chatMessageSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      index: true
+    },
+    role: {
+      type: String,
+      enum: ['user', 'model'],
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model('ChatMessage', chatMessageSchema);
